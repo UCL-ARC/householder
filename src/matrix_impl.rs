c040ac28f088d914c9be7716b088e32e76c5960a
Timo Betcke <timo.betcke@gmail.com>
2022-07-02 00:40:05 +0100

    Added missing file

--- src/matrix_impl.rs
+++ src/matrix_impl.rs
@@ -3,14 +3,20 @@
 use crate::matrix::Matrix;
 use crate::matrix_mut::MatrixMut;
 use crate::traits::*;
-use crate::types::{Scalar, IndexType};
+use crate::types::{IndexType, Scalar};
 
 macro_rules! methods {
     ($MatrixType:ident, $MatTrait:ident) => {
-        impl<Item: Scalar, L: LayoutIdentifier, RS: SizeIdentifier, CS: SizeIdentifier, 
-        MatImpl: $MatTrait<Item, L, RS, CS> >
-        LayoutIdentifier for $MatrixType<Item, MatImpl, L, RS, CS> {}
-        };
+        impl<
+                Item: Scalar,
+                L: LayoutIdentifier,
+                RS: SizeIdentifier,
+                CS: SizeIdentifier,
+                MatImpl: $MatTrait<Item, L, RS, CS>,
+            > LayoutIdentifier for $MatrixType<Item, MatImpl, L, RS, CS>
+        {
+        }
+    };
 }
 
 methods!(Matrix, MatrixTrait);
